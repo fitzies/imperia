bd73a69f369f1664a4cf77391ee03d84d26d7b6f
oli <olifitz1610@gmail.com>
2023-06-05 13:49:03 +0800

    init

---
+++ src/app/api/webhooks/user/route.ts
@@ -0,0 +1,18 @@
+import { headers } from "next/headers";
+import { Webhook } from "svix";
+
+async function handler(request: Request) {
+  const payload = await request.json();
+  const headersList = headers();
+  const heads = {
+    "svix-id": headersList.get("svix-id"),
+    "svix-timestamp": headersList.get("svix-timestamp"),
+    "svix-signature": headersList.get("svix-signature"),
+  };
+
+  const wh = new Webhook(JSON.stringify(payload), heads);
+}
+
+export const GET = handler;
+export const POST = handler;
+export const PUT = handler;
