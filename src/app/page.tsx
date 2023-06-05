1888fc71782070b2a16197e6ceac2bd9ff6021a4
oli <olifitz1610@gmail.com>
2023-06-05 16:25:13 +0800

    12

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+import { SignOutButton } from "@clerk/nextjs";
+
 const Page = ({ params }: { params: { slug: string } }) => {
   return (
     <div className="w-screen h-screen bg-base-100">
-      <button className="btn btn-secondary">hello</button>
+      <SignOutButton />
     </div>
   );
 };
