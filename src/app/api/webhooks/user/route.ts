4c8fd78ff89397848d2c29a1ef22e2c570c480d3
oli <olifitz1610@gmail.com>
2023-06-05 23:24:14 +0800

    all2

--- src/app/api/webhooks/user/route.ts
+++ src/app/api/webhooks/user/route.ts
@@ -1,35 +1,11 @@
 import type { WebhookEvent } from "@clerk/clerk-sdk-node";
-import { Webhook } from "svix";
-import { buffer } from "micro";
-import { NextRequest, NextResponse } from "next/server";
 
-// export const config = {
-//   api: {
-//     bodyParser: false,
-//   },
-// };
-
-const secret = "whsec_d68QWTo64dQyP97L++bkQ5qJaCbASlR0";
-
-async function handler(req: Request, res: Response) {
-  //@ts-ignore
-  const payload = (await buffer(req)).toString();
-  const headers = req.headers;
-
-  const wh = new Webhook(secret);
-  let msg;
-  try {
-    //@ts-ignore
-    msg = wh.verify(payload, headers);
-    return NextResponse.json({ msg });
-  } catch (err) {
-    // res.status(400).json({});
-    return NextResponse.json({ err });
+const handler = async (req: Request | any) => {
+  const evt = req.body.evt as WebhookEvent;
+  switch (evt.type) {
+    case "user.created": // this is typed
+      evt.data; // this is also typed
   }
+};
 
-  // Do something with the message...
-
-  // res.json({});
-}
-
-export { handler as POST, handler as GET, handler as PUT };
+export { handler as POST };
